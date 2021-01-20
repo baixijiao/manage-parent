602f07f83b451ab4d7df8041485237233240138f
白西郊 <492681225@qq.com>
2021-01-20 19:03:34 +0800

    Eureka注册中心开启密码认证

---
+++ manage-eureka/src/main/java/net/htjs/manage/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package net.htjs.manage.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Auther: baixijiao
+ * @Date：2021-01-20 18:59
+ * @Description： Security 配置类
+ * @Modified By:
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 关闭csrf
+        http.csrf().disable();
+        // 支持httpBasic
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
