452f17da1b32c7232556a023e9d1eac6cff3634d
白西郊 <492681225@qq.com>
2021-01-20 18:20:16 +0800

    Spring Boot Admin集成Eureka 由于Eureka版本过低，Admin及Spring Boot降低版本兼容

--- manage-admin/src/main/java/net/htjs/manage/admin/ManageAdminApplication.java
+++ manage-admin/src/main/java/net/htjs/manage/admin/ManageAdminApplication.java
@@ -3,7 +3,9 @@ package net.htjs.manage.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 public class ManageAdminApplication {
