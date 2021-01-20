e5fcdbd6b504401fbfd37c731b5f95d74fb6f7eb
白西郊 <492681225@qq.com>
2021-01-20 10:49:25 +0800

    初始提交

---
+++ manage-admin/src/test/java/net/htjs/manage/admin/ManageAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package net.htjs.manage.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
