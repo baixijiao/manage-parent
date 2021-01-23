60d5402bfaf089f48005c8010a17cbfa1436409d
白西郊 <492681225@qq.com>
2021-01-23 17:11:23 +0800

    添加Config配置中心服务端

---
+++ manage-config/src/main/java/net/htjs/manage/config/ManageConfigApplication.java
@@ -0,0 +1,17 @@
+package net.htjs.manage.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ManageConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageConfigApplication.class, args);
+    }
+
+}
---
+++ manage-config/src/test/java/net/htjs/manage/config/ManageConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package net.htjs.manage.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ManageConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
